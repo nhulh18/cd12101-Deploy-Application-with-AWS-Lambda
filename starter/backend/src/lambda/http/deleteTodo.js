68d32725ac3b910f80641d49d53aa262b90063db
nhulh18 <nhulee.h28@gmail.com>
2024-10-01 22:53:36 +0700

    Update source

--- starter/backend/src/lambda/http/deleteTodo.js
+++ starter/backend/src/lambda/http/deleteTodo.js
@@ -1,8 +1,27 @@
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import { getUserId } from '../utils.mjs'
+import { deleteTodoLogic } from '../../businessLogic/todos.mjs'
 
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
+  .handler(async (event) => {
+    const todoId = event.pathParameters.todoId
+    const userId = getUserId(event)
 
-  // TODO: Remove a TODO item by id
-  return undefined
-}
+    await deleteTodoLogic(userId, todoId)
 
+    return {
+      statusCode: 202,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify({})
+    }
+  })
--- starter/backend/src/lambda/http/updateTodo.js
+++ starter/backend/src/lambda/http/updateTodo.js
@@ -1,7 +1,26 @@
-export function handler(event) {
-  const todoId = event.pathParameters.todoId
-  const updatedTodo = JSON.parse(event.body)
-  
-  // TODO: Update a TODO item with the provided id using values in the "updatedTodo" object
-  return undefined
-}
+import middy from '@middy/core'
+import cors from '@middy/http-cors'
+import httpErrorHandler from '@middy/http-error-handler'
+import { getUserId } from '../utils.mjs'
+import { updateTodoLogic } from '../../businessLogic/todos.mjs'
+
+export const handler = middy()
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  )
+  .handler(async (event) => {
+    const todoId = event.pathParameters.todoId
+    const updatedTodo = JSON.parse(event.body)
+    const userId = getUserId(event)
+    const todo = await updateTodoLogic(userId, todoId, updatedTodo)
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      body: JSON.stringify(todo)
+    }
+  })
